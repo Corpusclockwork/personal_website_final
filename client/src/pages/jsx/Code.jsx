4775d34d97e27fc8b399e6ffa62c3d0cd1218eb2
Lamis McDowall-Rose <lamism@gmail.com>
2025-10-01 16:54:29 +0100

    Update descriptions

--- client/src/pages/jsx/Code.jsx
+++ client/src/pages/jsx/Code.jsx
@@ -15,7 +15,7 @@ function Code(){
                     </li>
                     <br />
                     <li className="project">
-                        <b className="projectName"><a href="https://alienroutelogger.com/" target="_blank">Climbing route tracker </a></b> 
+                        <b className="projectName"><a href="https://alienroutelogger.com/" target="_blank">Climbing route tracker</a></b> 
                         <div className="projectDescription"> A website where users can hypothetically log in and track the climbs they have done at the local climbing gym I work at, pretty sure this has been done before (see TopLogger), 
                             but I wanted a better understanding of setting up databases, as well as more experience in the backend. It's built using Javascript with Vue, Python with Django, and a MySQL database. I deployed it using DigitalOcean 
                             (wow, using that was a better experience than using AWS for deployment, might change over to host my personal website there at some point), and used apache2 to serve it, along with CertBot for the SSL certificate.
@@ -25,18 +25,23 @@ function Code(){
                     </li>
                     <br />
                     <li className="project">
-                        <b className="projectName"> <a href="https://github.com/Corpusclockwork/VueImageMarker" target="_blank">Image Marker</a><b className="projectState"> (IN PROGRESS)</b></b> 
+                        <b className="projectName"> <a href="https://github.com/Corpusclockwork/VueImageMarker" target="_blank">ImageMarker</a></b> 
                         <div className="projectDescription"> I made a Vue equivalent of <a href="https://www.npmjs.com/package/react-image-marker" target="_blank">this</a> (or an alternative version of <a href="https://github.com/jarvisniu/vue-image-marker" target="_blank">this</a> I guess) 
-                            for the climbing website mentioned above. I'm making it into a npm package, because one, it's good for me as a developer to think about how to make my code reusable and useful for other people, and two,
-                            because I am very generous and kind :)))). I'm structuring this project using Vue Composition API, which is apparently the way forward when it comes to Vue. Can't say I'm a fan yet, but at least I have some experience in it now.
- 
+                            for the climbing website mentioned above. I've remade this component using the Vue Composition API, which is apparently the way forwards when it comes to Vue. 
+                            I had never used the Vue Composition API before, so built this to get a feel for it.
                         </div>
                     </li>
                     <br />
                     <li className="project">
                         <b className="projectName">Website for a friend<b className="projectState"> (IN PROGRESS)</b></b> 
-                        <div className="projectDescription"> One of my friends wanted a website for *her* art, and it gave me an excuse to use Typescript, which I hadn't done before. Honestly I wanted a nice chill frontend project, as my next project is going to be in C.
-                            
+                        <div className="projectDescription"> One of my friends wanted a website for *her* art, and it gave me an excuse to use Typescript, which I hadn't done before.   
+                        </div>
+                    </li>
+                    <br />
+                    <li className="project">
+                        <b className="projectName"><a href="https://github.com/Corpusclockwork/C_database" target="_blank">Using csv files to construct a simple database using C</a><b className="projectState"> (IN PROGRESS)</b></b> 
+                        <div className="projectDescription"> I hadn't used C since being at university, and thought it would be fun to relearn. I want to be able to add and delete rows, as well as perform searches, and joins.
+                            Making my small database able to handle files sent over sockets would be nice functionality to include.
                         </div>
                     </li>
                 </ul>
